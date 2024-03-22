d85205f0ce11ffa7dac61fe0d9822e9cc0b933f4
Rahul Patni <rahul20patni@gmail.com>
2024-03-21 18:48:17 -0700

    initial setup

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+        Rahul Patni
+        <a href="mailto:therahulpatni@gmail.com" className="text-blue-600">therahulpatni@gmail.com</a>
+        <a href="https://linkedin.com/in/rahulpatni" className="text-blue-600">Linkedn</a>
+        <a href="https://github.com/patnir" className="text-blue-600">Github</a>
+      </div>
+    </main>
+  );
+}
