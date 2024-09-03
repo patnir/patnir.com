514d233d95f6931d0cce861763bf64fdbca4e429
Rahul Patni <rahul20patni@gmail.com>
2024-09-02 21:37:01 -0700

    substack comments lol

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,16 @@ export default function Home() {
   return (
     <main>
       <div className="container relative">
-        <p>
-          I live in Seattle. I work at <a href="https://ironfish.network" target="_blank" className="text-blue-600">Ironfish</a>.
-        </p>
-        <p>Previously, I worked at <a href="https://atomic.vc" target="_blank" className="text-blue-600">Atomic</a>, <a href="https://ridgelineapps.com" target="_blank" className="text-blue-600">Ridgeline</a>, and Microsoft.</p>
-        <p className="py-4">I write about work at <a href="https://foundingengineer.com" target="_blank" className="text-blue-600">foundingengineer.com</a>.</p>
-        <p>
-          <a href="https://www.x.com/iAmRahulPatni/" target="_blank" className="text-blue-600">Twitter</a> is the best place for us to connect.
-        </p>
+        <div className="pr-4">
+          <p>
+            I live in Seattle. I work at <a href="https://ironfish.network" target="_blank" className="text-blue-600">Ironfish</a>.
+          </p>
+          <p>Previously, I worked at <a href="https://atomic.vc" target="_blank" className="text-blue-600">Atomic</a>, <a href="https://ridgelineapps.com" target="_blank" className="text-blue-600">Ridgeline</a>, and Microsoft.</p>
+          <p className="py-4">I write about work at <a href="https://foundingengineer.com" target="_blank" className="text-blue-600">foundingengineer.com</a>.</p>
+          <p>
+            <a href="https://www.x.com/iAmRahulPatni/" target="_blank" className="text-blue-600">Twitter</a> is the best place for us to connect.
+          </p>
+        </div>
       </div>
     </main >
   );
--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -13,6 +13,7 @@ export function SiteFooter() {
   return (
     <footer className="py-6">
       <div className="container flex items-center gap-4">
+        {/* <iframe src="https://foundingengineer.substack.com/embed" className="w-full h-[200px] bg-white"></iframe> */}
         <p className="text-balance text-left text-sm leading-loose text-muted-foreground">
           Built by{" "}
           <a
@@ -23,7 +24,7 @@ export function SiteFooter() {
           >
             me
           </a>
-          . The source code is available on{" "}
+          . You can find all of my work on{" "}
           <a
             href={siteConfig.links.github}
             target="_blank"
