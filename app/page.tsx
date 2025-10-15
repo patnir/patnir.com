9e250dbb5d0f6af65551f15120fd0ee908268397
Rahul Patni <rahul20patni@gmail.com>
2025-10-14 21:03:10 -0700

    update ironfish update

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
     <div className="container relative">
       <div className="pr-4">
         <p>
-          I live in Seattle. I work at <a href="https://ironfish.network" target="_blank" className="text-blue-600">Ironfish</a> (backed by a16z, sequoia). Previously, I worked at <a href="https://atomic.vc" target="_blank" className="text-blue-600">Atomic</a>, <a href="https://ridgelineapps.com" target="_blank" className="text-blue-600">Ridgeline</a>, and Microsoft.
+          I live in Seattle. I work at Coinbase. I joined via acquisition when I worked at <a href="https://www.coinbase.com/blog/Coinbase-acquires-team-to-accelerate-privacy-efforts-on-Base" target="_blank" className="text-blue-600">Iron Fish</a> (backed by a16z, sequoia). Previously, I worked at <a href="https://atomic.vc" target="_blank" className="text-blue-600">Atomic</a>, <a href="https://ridgelineapps.com" target="_blank" className="text-blue-600">Ridgeline</a>, and Microsoft.
         </p>
         <p className="py-2">I write about engineering at startups on my <a href="https://blog.foundingengineer.com" target="_blank" className="text-blue-600">blog</a>.</p>
         <p className="pb-2">
